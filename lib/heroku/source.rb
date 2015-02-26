fa434f591c5bf48d81cd8e5c6da3e2a92108161f
Martin N <mr.avgp@googlemail.com>
2015-02-26 23:23:31 +0000

    Alright, we should get the source URLs

--- lib/heroku/source.rb
+++ lib/heroku/source.rb
@@ -5,8 +5,11 @@ class Source
         @api_key = api_key
         url = URI.parse("https://#{api_key}@api.heroku.com/apps/#{app}/sources")
 
-        req = Net::HTTP::Get.new(url.to_s)
-        res = Net::HTTP.start(url.host, url.port) {|http|
+        req = Net::HTTP::Post.new(url.request_uri)
+        req.add_field("Accept", "application/vnd.heroku+json; version=3")
+        req.add_field("Authorization", "Bearer #{api_key}")
+
+        res = Net::HTTP.start(url.host, url.port, nil, nil, nil, nil, {use_ssl: true}) {|http|
           http.request(req)
         }
         puts res.body
