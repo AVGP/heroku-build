04240485bc56f15ca7e6b57774a09d27a3759408
Martin N <mr.avgp@googlemail.com>
2015-02-26 22:54:35 +0000

    Trying to get to the sources endpoint

--- lib/heroku/build.rb
+++ lib/heroku/build.rb
@@ -1,7 +1,11 @@
 require 'heroku/command/base'
+require_relative 'source'
 
 class Heroku::Command::Build < Heroku::Command::BaseWithApp
   def index
-    display "Usage: heroku build"
+    tarball_path = ARGV[-1]
+    display "Usage: heroku build <path/to/tarball>"
+    display "Building #{app} from #{tarball_path}"
+    source = Source.new(tarball_path, app, Heroku::Auth.password)
   end
 end
---
+++ lib/heroku/source.rb
@@ -0,0 +1,14 @@
+require "net/https"
+
+class Source
+    def init(tarball_path, app, api_key)
+        @api_key = api_key
+        url = URI.parse("https://#{api_key}@api.heroku.com/apps/#{app}/sources")
+
+        req = Net::HTTP::Get.new(url.to_s)
+        res = Net::HTTP.start(url.host, url.port) {|http|
+          http.request(req)
+        }
+        puts res.body
+    end
+end
