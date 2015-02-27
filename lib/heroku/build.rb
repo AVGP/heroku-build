9f24c9cece863c314f7e6d8234a7fb521d4bfd46
Martin N <mr.avgp@googlemail.com>
2015-02-27 00:35:32 +0000

    Well, NOW it should work

--- lib/heroku/build.rb
+++ lib/heroku/build.rb
@@ -6,24 +6,33 @@ require_relative 'source'
 
 class Heroku::Command::Build < Heroku::Command::BaseWithApp
   def index
-    tarball_path = option[:src]
-    version = option[:version]
-    display "Usage: heroku build <path/to/tarball>"
-    display "Building #{app} from #{tarball_path}"
+    tarball_path = args.shift
+    version = args.shift
+    if tarball_path.nil? || version.nil?
+      display "Usage: heroku build <path/to/tarball> <version>"
+      return
+    end
+
+    display "Building #{app} #{version} from #{tarball_path}"
     source = Source.new(app, Heroku::Auth.password)
-    source_url = source.upload(tarball_path)
-    make_build_from_url(source_url, version)
+    tarball_url = source.upload(tarball_path)
+    make_build_from_url(tarball_url, version)
+  end
+
+  def src
+    src = options[:src]
   end
 
   private
 
-  def make_build_from_url(url, version)
+  def make_build_from_url(tar_url, version)
     url = URI.parse("https://api.heroku.com/apps/#{app}/builds")
 
     req = Net::HTTP::Post.new(url.request_uri)
     req.add_field("Accept", "application/vnd.heroku+json; version=3")
     req.add_field("Authorization", "Bearer #{Heroku::Auth.password}")
-    req.body = "[ #{ { source_blob: { url: url, version: version}}.to_json } ]"
+    req.add_field("Content-Type", "application/json")
+    req.body = "#{ { source_blob: { url: tar_url, version: version}}.to_json }"
 
     res = Net::HTTP.start(url.host, url.port, nil, nil, nil, nil, {use_ssl: true}) {|http|
       http.request(req)
